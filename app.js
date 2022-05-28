67f66cc7f641a60cb8ba89ce56cf56b54753a478
Ashish <ashish@Ashishs-MacBook.local>
2022-05-28 13:38:00 +0530

    added Procfile

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get("/", function (req, res) {
   res.send("Hello");
 });
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000.");
 });
