6e9f45150fc16f6833d0f77815e71ad11f4f12e6
Ashish <ashish@Ashishs-MacBook.local>
2022-05-30 00:52:17 +0530

    adding img comparison

--- app.js
+++ app.js
@@ -3,24 +3,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const path = require("path");
+const basePath = path.join(__dirname, "/public");
+console.log(basePath);
+const app = express();
+
+app.use(express.static(basePath));
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-const app = express();
-
-app.use(express.static("public"));
-
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.post("/", urlencodedParser, function (req, res) {
+  res.sendFile(__dirname + "/public/imgcapture.html");
 });
 
-app.get("/imgcapture", function (req, res) {});
-
-app.post("/", urlencodedParser, function (req, res) {
-  var fullName = req.body.fName;
-
-  res.sendFile(__dirname + "/imgcapture.html");
-  //   res.sendFile(__dirname + "/imgcapture.html");
+app.post("/imgcompare", function (req, res) {
+  res.sendFile(__dirname + "/public/imgcompare.html");
 });
 
 app.listen(process.env.PORT || 3000, function () {
