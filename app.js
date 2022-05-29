b233eab63de63b2da260220b8f284fc5f8cd86ae
Ashish <ashish@Ashishs-MacBook.local>
2022-05-29 12:37:58 +0530

    added name and imgcapture

--- app.js
+++ app.js
@@ -2,11 +2,22 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+const ejs = require("ejs");
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  res.send("Hello");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", urlencodedParser, function (req, res) {
+  var fullName = req.body.fName;
+  console.log(fullName);
+  res.sendFile(__dirname + "/imgcapture.html");
 });
 
 app.listen(process.env.PORT || 3000, function () {
